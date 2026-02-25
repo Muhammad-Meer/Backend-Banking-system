51ea288818d060f0759c614d92f3caafa6c8fd66
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-25 20:09:43 +0500

    update

--- service.js
+++ service.js
@@ -1,12 +1,13 @@
 const app = require('./src/app')
+const conectDB = require('./src/config/db')
+require("dotenv").config()
+
+
+conectDB()
 
 
 
-const port = 3200
-
-
-
-
+ const port = 3200 
 app.listen(port , () => {
-  console.log("http://localhost"+ port)
+  console.log("http://localhost:"+ port)
 })
--- src/config/db.js
+++ src/config/db.js
@@ -2,5 +2,17 @@ const mongoose = require('mongoose')
 
 
 const conectDB = async() => {
-    mongoose.connect(process.env.MONGO_URI)
+     await mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("conectDB")
+    })
+    .catch((error) => {
+      console.log("eroor mongo nahi conect ho raha ha " + error)
+     process.exit(1)
+
+    })
+
 }
+
+module.exports = conectDB
+
