bd30f36a07c3668d33d0aabafe635c0e05ff075e
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-26 18:44:12 +0800

    smit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,9 +3,28 @@ const usermodel = require('../models/model')
 
 
 
-function userregistercontroller(req , res ) {
+ async function userregistercontroller(req , res ) {
 
    const {email , name,password } = req.body
+
+
+   const isuserexist = await usermodel.findOne({
+    email: email
+   })
+
+   if(isuserexist) {
+    return res.status(422).json({
+      message: "user alredy exist",
+      status: "failed"
+    })
+   }
+
+   const createuser = await usermodel.create({
+    email, password, name
+   })
+
+
+   
 
 }
 
