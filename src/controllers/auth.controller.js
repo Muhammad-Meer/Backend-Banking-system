d30870b13a164b43b26e24c75c0d740dc65c5bc9
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-27 20:42:15 +0500

    new code

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,47 +1,84 @@
-const usermodel = require('../models/model')
-const jwt = require('jsonwebtoken')
-
+const usermodel = require("../models/model");
+const jwt = require("jsonwebtoken");
 
 /* post   /api/auth/register        */
 
- async function userregistercontroller(req , res ) {
+async function userregistercontroller(req, res) {
+  const { email, name, password } = req.body;
 
-   const {email , name,password } = req.body
+  const isuserexist = await usermodel.findOne({
+    email: email,
+  });
 
-
-   const isuserexist = await usermodel.findOne({
-    email: email
-   })
-
-   if(isuserexist) {
+  if (isuserexist) {
     return res.status(422).json({
       message: "user alredy exist",
-      status: "failed"
+      status: "failed",
+    });
+  }
+
+  const createuser = await usermodel.create({
+    email,
+    password,
+    name,
+  });
+
+  const jwttoken = jwt.sign(
+    { userId: createuser._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "5d" },
+  );
+
+  res.cookie("token", jwttoken);
+
+  res.status(201).json({
+    userdeta: {
+      _id: createuser._id,
+      email: createuser.email,
+      name: createuser.name,
+    },
+    jwttoken,
+  });
+}
+
+
+async function userrlogincontroller(req, res) {
+  const { email, password } = req.body;
+
+  const useremail= await usermodel.findOne({email})
+
+
+  if(!useremail) {
+    return res.status(401).json({
+      message: "email or password invalid"
     })
-   }
+  }
 
-   const createuser = await usermodel.create({
-    email, password, name
-   })
+  const isuservalid = await user.comparePassword(password)
 
+  if(!isuservalid) {
+    return res.status(401).json({
+      message: "email or password is unvalid"
+    })
+  }
 
-   const jwttoken = jwt.sign({userId: createuser._id}, process.env.JWT_SECRET, {expiresIn: "5d"})
-
-   res.cookie("token",jwttoken)
-
-   res.status(201).json({
-       userdeta: {
-        _id: createuser._id,
-        email: createuser.email,
-        name: createuser.name,
-       },
-       jwttoken
-   })
-}
-
-
-async function userrlogincontroller(req , res) {
   
-}
+  const jwttoken = jwt.sign(
+    { userId: createuser._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "5d" },
+  );
 
-module.exports  = {userregistercontroller,userrlogincontroller}
+  res.cookie("token", jwttoken);
+
+  res.status(200).json({
+    userdeta: {
+      _id: createuser._id,
+      email: createuser.email,
+      name: createuser.name,
+    },
+    jwttoken,
+  });
+} 
+
+module.exports = { userregistercontroller, userrlogincontroller };
