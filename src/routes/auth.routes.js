38efb45649afcd465cc48ac37d9bdd654a47e108
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-27 20:12:46 +0800

    updste is here

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,17 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const userregistercontroller = require('../controllers/auth.controller')
 
+const { userregistercontroller, userrlogincontroller } = require('../controllers/auth.controller')
 
-
-
-/* post   /api/auth/register        */
 router.post('/register', userregistercontroller)
-
-
-
-/* post   /api/auth/register        */
 router.post('/login', userrlogincontroller)
 
 module.exports = router
-
